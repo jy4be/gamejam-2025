5112a78b21f0ebfb2e7c924e9428f017fa371d23
Janica <jani.voelzke@web.de>
2025-10-25 22:41:36 +0200

    Shot Part 1

---
+++ Scripts/Effects/EffectShot.gd
@@ -0,0 +1,31 @@
+
+extends IEffect
+class_name EffectShot
+
+var origin:Vector2i = Vector2i(-1,-1)
+var highlight:Array[Vector2i] = []
+
+func intern_onStart(primaryTile : Vector2i, secondaryTile : Vector2i) -> Array[Vector2i]:
+	origin = primaryTile
+	return GlobalVariables.map.getNeighbors(primaryTile)
+
+func onSelection(selectedTile : Vector2i):
+	pass
+	
+func onHighlight(tileUnderMouse : Vector2i):
+	for tile:Tile in highlight.map(GlobalVariables.map.getTile): 
+		tile.setStateFlag(Tile.TILE_STATE.HOVERED,false)
+	var highlight:Array[Vector2i] = GlobalVariables.map.getLine(origin, tileUnderMouse)
+	for tile:Tile in highlight.map(GlobalVariables.map.getTile): 
+		tile.setStateFlag(Tile.TILE_STATE.HOVERED,true)
+	
+func getSpritePath()->String:
+	return "res://Assets/Sandagon.png"
+	
+func getSpritePathBackGround()->String:
+	return ""
+
+func isTeamEffect() -> bool:
+	return false
+func getName()->String:
+	return "EffectShot"
