04b04b12f0e28434d51f571a88da7b6a63362c69
Janica <jani.voelzke@web.de>
2025-10-25 23:48:05 +0200

    don't trigger tile in walking effect

--- Scripts/IEffect.gd
+++ Scripts/IEffect.gd
@@ -4,9 +4,9 @@
 # returns Array<Vector2i> 
 func onStart(primaryTile : Vector2i, secondaryTile : Vector2i) -> Array[Vector2i]:
 	if !isTeamEffect() || getUnitOnTile(primaryTile).controller == getUnitOnTile(secondaryTile).controller :
-		if primaryTile != Vector2i(-1, -1):
+		if primaryTile != Vector2i(-1, -1) && getName() == GlobalVariables.map.getTile(primaryTile).tileEffect.getName():
 			GlobalVariables.map.getTile(primaryTile).setStateFlag(Tile.TILE_STATE.ALREADY_TRIGGERED, true)
-		if secondaryTile != Vector2i(-1, -1):
+		if secondaryTile != Vector2i(-1, -1)&& getName() == GlobalVariables.map.getTile(secondaryTile).tileEffect.getName():
 			GlobalVariables.map.getTile(secondaryTile).setStateFlag(Tile.TILE_STATE.ALREADY_TRIGGERED, true)
 		return intern_onStart(primaryTile,secondaryTile)
 	return []
