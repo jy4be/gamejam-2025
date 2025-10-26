62eccfaeb057d65d08098bafd7b152d6c9836db7
Janica <jani.voelzke@web.de>
2025-10-26 12:49:17 +0100

    Falle + ...

---
+++ Scripts/Effects/EffectFalle.gd
@@ -0,0 +1,28 @@
+extends IEffect
+class_name EffectFalle
+func intern_onStart(primaryTile : Vector2i, secondaryTile : Vector2i) -> Array[Vector2i]:
+	for tileIndex in [primaryTile, secondaryTile]:
+		var index = GlobalVariables.units.find_custom(func (unit : Unit): 
+			return unit.currentOccupiedTileIndex == tileIndex && !unit.hasMoved)
+		if index != -1 :
+			GlobalVariables.units[index].health -= 1
+	onEnd()
+	return []
+
+func onSelection(selectedTile : Vector2i):
+	pass
+	
+func onHighlight(tileUnderMouse : Vector2i):
+	pass
+	
+func getSpritePath()->String:
+	return "res://Assets/Falle.png"
+
+func getSpritePathBackGround()->String:
+	return "res://Assets/Grasagon.png"
+
+func isTeamEffect() -> bool:
+	return false
+
+func getName() -> String:
+	return "Falle"
